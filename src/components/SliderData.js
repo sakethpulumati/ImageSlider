6fc8caf5be6b4d0a26e1f38a15b51ed963de6dd9
unknown <sakethp1998@gmail.com>
2021-08-07 15:07:29 +0530

    deployed

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: "https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1526786220381-1d21eedf92bf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1552055568-f8c4fb8c6320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1539807083969-fc1f6a4f6da8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/flagged/photo-1559717201-fbb671ff56b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+    }
+]
